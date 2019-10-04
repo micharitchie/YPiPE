634d51145c84c38cf70536650a251e919d6e30e7
Michael <ritchier@usc.edu>
2019-10-04 12:14:05 -0700

    Updating virutal scene

---
+++ YPiPE/Assets/Scripts/HouseCtrl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseCtrl : MonoBehaviour
+{
+    public Sprite openDoor;
+    public Sprite closedDoor;
+
+    SpriteRenderer mySpriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.name == "Rory")
+        {
+            mySpriteRenderer.sprite = openDoor;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.name == "Rory")
+        {
+            mySpriteRenderer.sprite = closedDoor;
+        }
+    }
+}
