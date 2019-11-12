a9c396f651b3e98665bab34348c175b3bc381def
Yvette Lo <yvettelo@usc.edu>
2019-11-12 03:39:42 -0800

    Added basic UI for body swapping - User can currently swap body parts on UI but needs to be hooked up to model

---
+++ YPiPE/Assets/Scripts/PartSwapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PartSwapper : MonoBehaviour
+{
+    int slotAmount = 6; //Hard-coded to the number of limbs we have to work with
+    public List<BodyPartSlot> slots = new List<BodyPartSlot>();
+    public List<BodyPartData> bodyParts = new List<BodyPartData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PartSwapper needs to be propagated with all of the specific slots and the current body parts in them
+
+        //Should also have a way to communicate with the actual model that will be displayed
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
