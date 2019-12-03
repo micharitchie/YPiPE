a69b2d126b6cc190825f09f10f4dbc54bec337e0
Michael <ritchier@usc.edu>
2019-12-02 16:41:49 -0800

    Building out tutorial for body swap

---
+++ YPiPE/Assets/Scripts/DoorFader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorFader : MonoBehaviour
+{
+
+	public string animState;
+	private Animator doorAnimator;
+
+	void Start()
+	{
+		doorAnimator = GetComponent<Animator>();
+	}
+
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		doorAnimator.SetFloat("Direction", 1);
+		doorAnimator.Play(animState);
+	}
+
+	
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		doorAnimator.SetFloat("Direction", -1);
+		doorAnimator.Play(animState);
+	}
+
+	public void StopAnimationSpeed()
+	{
+		doorAnimator.SetFloat("Direction", 0);
+
+	}
+}
---
+++ YPiPE/Assets/Scripts/PostProcessingFade.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using RetroLookPro.Enums;
+
+public class PostProcessingFade : MonoBehaviour
+{
+
+    //private PostProcessVolume activeVolume;
+    private RLProColormapPalette CMPVolume;
+    private bool fadeIn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //CMPVolume = Get(Shader.Find("RetroLookPro/ColorPalette"));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PixelsFade()
+    {
+        CMPVolume.pixelSize.value = 100;
+    }
+}
