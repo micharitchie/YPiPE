7899745e507c6b6930873d54969f0dded7276e05
Michael <ritchier@usc.edu>
2020-02-12 13:30:56 -0800

    Building out Narrative

---
+++ YPiPE/Assets/Scripts/Tripwire.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class Tripwire : MonoBehaviour
+{
+    public Flowchart myFlowchart;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        myFlowchart.ExecuteBlock("Tripwire");
+    }
+}
