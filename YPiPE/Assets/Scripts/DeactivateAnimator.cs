fe684f0d722af2c16d16906c8eb333bd6575c927
Michael <ritchier@usc.edu>
2020-06-04 18:18:05 -0700

    Adding Roomswaps
    Setting up the room swap mechanic to work in all rooms

---
+++ YPiPE/Assets/Scripts/DeactivateAnimator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateAnimator : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.enabled = false;
+    }
+
+    public void setAnim(bool animActive)
+    {
+        anim.enabled = animActive;
+    }
+
+}
--- YPiPE/Assets/Scripts/InteractableObjects.cs
+++ YPiPE/Assets/Scripts/InteractableObjects.cs
@@ -7,40 +7,22 @@ public class InteractableObjects : MonoBehaviour
 {
 
 	public GameObject lookUI;
-    public Transform setLocation;
+    //public Transform setLocation;
     public string fungusBool;
     public Flowchart targetFlowchart;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
-        //Vector3 buttonLocation = new Vector3(setLocation.position.x + 2.3f, setLocation.position.y + 4.2f, setLocation.position.z);
-        //lookUI.transform.position = buttonLocation;
         lookUI.SetActive(true);
         if (targetFlowchart != null)
         {
             targetFlowchart.SetBooleanVariable(fungusBool, true);
         }
     }
-    /*private void OnTriggerStay2D(Collider2D collision)
-    {
-        Vector3 buttonLocation = new Vector3(setLocation.position.x + 2.3f, setLocation.position.y + 4.2f, setLocation.position.z);
-        lookUI.transform.position = buttonLocation;
-    }*/
+    
     private void OnTriggerExit2D(Collider2D collision)
     {
         lookUI.SetActive(false);
-        //lookIndicator.SetActive(false);
         if (targetFlowchart != null)
         {
             targetFlowchart.SetBooleanVariable(fungusBool, false);
